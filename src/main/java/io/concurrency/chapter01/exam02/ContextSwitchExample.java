939d63d461209a437aa53ac255911ab869a87934
ih.lee <ih.lee@okestro.com>
2025-01-20 23:07:01 +0900

    feat: 스레드 컨텍스트 스위칭 처리 테스트

--- src/main/java/io/concurrency/chapter01/exam02/ContextSwitchExample.java
+++ src/main/java/io/concurrency/chapter01/exam02/ContextSwitchExample.java
@@ -3,8 +3,9 @@ package io.concurrency.chapter01.exam02;
 public class ContextSwitchExample {
     public static void main(String[] args) {
 
+        // 스레드 1,2,3이 컨텍스트 스위칭을 통해 cpu를 할당 받아 처리하게 된다.
         Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 3; i++) {
                 System.out.println("Thread 1: " + i);
                 try {
                     Thread.sleep(1);
@@ -15,7 +16,7 @@ public class ContextSwitchExample {
         });
 
         Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 3; i++) {
                 System.out.println("Thread 2: " + i);
                 try {
                     Thread.sleep(1);
@@ -26,7 +27,7 @@ public class ContextSwitchExample {
         });
 
         Thread thread3 = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 3; i++) {
                 System.out.println("Thread 3: " + i);
                 try {
                     Thread.sleep(1);
