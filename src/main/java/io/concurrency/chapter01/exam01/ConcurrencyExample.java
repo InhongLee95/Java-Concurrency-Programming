84d3b90e5538f4912da389c4d2295752ecb957df
ih.lee <ih.lee@okestro.com>
2025-01-20 22:39:18 +0900

    feat: 병렬처리 테스트1

--- src/main/java/io/concurrency/chapter01/exam01/ConcurrencyExample.java
+++ src/main/java/io/concurrency/chapter01/exam01/ConcurrencyExample.java
@@ -6,18 +6,19 @@ import java.util.List;
 public class ConcurrencyExample {
     public static void main(String[] args) {
 
+        // cpu 코어 수
         int cpuCores = Runtime.getRuntime().availableProcessors() * 2;
 //        int cpuCores = 13;
 
-        // CPU 개수를 초과하는 데이터를 생성
+        // CPU 개수만큼 데이터를 생성
         List<Integer> data = new ArrayList<>();
         for (int i = 0; i < cpuCores; i++) {
             data.add(i);
         }
 
-        // CPU 개수를 초과하는 데이터를 병렬로 처리
-        long startTime2 = System.currentTimeMillis();
-        long sum2 = data.parallelStream()
+        // CPU 개수만큼 데이터를 각각의 스레드로 병렬 처리
+        long startTime1 = System.currentTimeMillis();
+        long sum1 = data.parallelStream()
                 .mapToLong(i -> {
                     try {
                         Thread.sleep(500);
@@ -28,9 +29,9 @@ public class ConcurrencyExample {
                 })
                 .sum();
 
-        long endTime2 = System.currentTimeMillis();
-
-        System.out.println("CPU 개수를 초과하는 데이터를 병렬로 처리하는 데 걸린 시간: " + (endTime2 - startTime2) + "ms");
-        System.out.println("결과2: " + sum2);
+        long endTime1 = System.currentTimeMillis();
+        System.out.println("cpu 코어수 " + cpuCores);
+        System.out.println("CPU 개수를 초과하는 데이터를 병렬로 처리하는 데 걸린 시간: " + (endTime1 - startTime1) + "ms");
+        System.out.println("결과1: " + sum1);
     }
 }
