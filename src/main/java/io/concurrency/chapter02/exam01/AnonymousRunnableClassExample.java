55151667165c2e8d202f34eced5a41611fa29f9b
leeinhong <leeinhong9512@gmail.com>
2025-01-26 07:07:56 +0900

    feat: 스레드 생성하는 방법 종류 테스트

--- src/main/java/io/concurrency/chapter02/exam01/AnonymousRunnableClassExample.java
+++ src/main/java/io/concurrency/chapter02/exam01/AnonymousRunnableClassExample.java
@@ -2,7 +2,7 @@ package io.concurrency.chapter02.exam01;
 
 public class AnonymousRunnableClassExample {
     public static void main(String[] args) {
-
+        // Runnable 및 익명 클래스를 이용하여 스레드 생성, 일회성
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
--- src/main/java/io/concurrency/chapter02/exam01/LambdaThreadExample.java
+++ src/main/java/io/concurrency/chapter02/exam01/LambdaThreadExample.java
@@ -2,6 +2,7 @@ package io.concurrency.chapter02.exam01;
 
 public class LambdaThreadExample {
     public static void main(String[] args) {
+        // 람다 표현식 이용하여 스레드 생성 방식
         Thread thread = new Thread(() -> System.out.println(Thread.currentThread().getName() + ": 스레드 실행 중.."));
         thread.start();
     }
