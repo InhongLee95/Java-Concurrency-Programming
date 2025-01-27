1e7030d86bf66b9bfd4b1e6e773964ae4a822323
leeinhong <leeinhong9512@gmail.com>
2025-01-27 15:26:13 +0900

    feat: 데몬 스레드 동작원리 테스트

--- src/main/java/io/concurrency/chapter04/exam03/UserAndDaemonInheritanceExample.java
+++ src/main/java/io/concurrency/chapter04/exam03/UserAndDaemonInheritanceExample.java
@@ -8,6 +8,7 @@ public class UserAndDaemonInheritanceExample {
             Thread childOfUserThread = new Thread(() -> {
                 System.out.println("사용자 스레드의 자식 스레드의 데몬 상태: " + Thread.currentThread().isDaemon());
             });
+            // 자식 스레드 실행
             childOfUserThread.start();
             System.out.println("사용자 스레드의 데몬 상태: " + Thread.currentThread().isDaemon());
         });
@@ -17,11 +18,15 @@ public class UserAndDaemonInheritanceExample {
             Thread childOfDaemonThread = new Thread(() -> {
                 System.out.println("데몬 스레드의 자식 스레드의 데몬 상태: " + Thread.currentThread().isDaemon());
             });
+            // 자식 스레드 실행 -> 데몬스레드는 데몬 스레드를 생성하게 된다.
             childOfDaemonThread.start();
             System.out.println("데몬 스레드의 데몬 상태: " + Thread.currentThread().isDaemon());
         });
+
+        // 데몬 스레드 ture 설정
         daemonThread.setDaemon(true);
 
+        // 각 스레드 실행
         userThread.start();
         daemonThread.start();
     }
--- src/main/java/io/concurrency/chapter04/exam03/UserThreadLifecycleExample.java
+++ src/main/java/io/concurrency/chapter04/exam03/UserThreadLifecycleExample.java
@@ -2,6 +2,7 @@ package io.concurrency.chapter04.exam03;
 
 public class UserThreadLifecycleExample {
     public static void main(String[] args) throws InterruptedException {
+
         // 사용자 스레드 1 생성
         Thread userThread1 = new Thread(() -> {
             for (int i = 0; i < 3; i++) {
@@ -28,12 +29,14 @@ public class UserThreadLifecycleExample {
             System.out.println("사용자 스레드 2 종료.");
         });
 
+
+        //스레드 실행
         userThread1.start();
         userThread2.start();
 
         // 메인 스레드가 userThread1과 userThread2의 종료를 기다립니다.
-//        userThread1.join();
-//        userThread2.join();
+        userThread1.join();
+        userThread2.join();
 
         System.out.println("모든 사용자 스레드가 종료되었습니다. 메인 스레드 종료.");
     }
