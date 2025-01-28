936600d33ea0dc0e6752632ef8bbe509078abd9f
leeinhong <leeinhong9512@gmail.com>
2025-01-28 12:40:28 +0900

    feat: 단일 스레드 및 멀티 스레드 동작 원리 테스트

--- src/main/java/io/concurrency/chapter05/exam01/MultiThreadExample.java
+++ src/main/java/io/concurrency/chapter05/exam01/MultiThreadExample.java
@@ -7,6 +7,8 @@ public class MultiThreadExample {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
 
+
+        // 스레드 1 생성
         Thread thread1 = new Thread(() -> {
             for (int i = 1; i <= 500; i++) {
                 synchronized (lock) {
@@ -20,6 +22,7 @@ public class MultiThreadExample {
             }
         });
 
+        // 스레드 2 생성
         Thread thread2 = new Thread(() -> {
             for (int i = 501; i <= 1000; i++) {
                 synchronized (lock) {
@@ -27,7 +30,7 @@ public class MultiThreadExample {
                 }
                 try {
                     Thread.sleep(1);
-                    throw new RuntimeException("error");
+//                    throw new RuntimeException("error");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -44,6 +47,8 @@ public class MultiThreadExample {
             e.printStackTrace();
         }
 
+        // 다른 스레드가 예외발생되도 다른 스레드 작업이 실행되지 못하는 상황은 발생되지 않는다.
+        // 멀티 스레드로 1~1000 sum 작업 소요 시간 1초 절반으로 줄었다.
         System.out.println("합계: " + sum);
         System.out.println("멀티 스레드 처리 시간: " + (System.currentTimeMillis() - start) + "ms");
     }
--- src/main/java/io/concurrency/chapter05/exam01/SingleThreadExample.java
+++ src/main/java/io/concurrency/chapter05/exam01/SingleThreadExample.java
@@ -5,6 +5,8 @@ public class SingleThreadExample {
         long start = System.currentTimeMillis();
 
         int sum = 0;
+
+        // 메인 스레드 단일로 처리
         for (int i = 1; i <= 1000; i++) {
             sum += i;
             try {
@@ -15,6 +17,8 @@ public class SingleThreadExample {
             }
         }
 
+        // 예외 발생 시 app이 실행되지 못하고 종료된다.
+        // 단일 스레드는 1~ 1000까지 sum 하는데 소요 시간 2초
         System.out.println("합계: " + sum);
         System.out.println("싱글 스레드 처리 시간: " + (System.currentTimeMillis() - start) + "ms");
     }
