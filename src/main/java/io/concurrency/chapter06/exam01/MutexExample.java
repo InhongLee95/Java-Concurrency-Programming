e0fef05cec2960d73094752e1a4e6cbc7ef1d662
leeinhong <leeinhong9512@gmail.com>
2025-01-28 15:34:50 +0900

    feat: 뮤텍스 락 메커니즘 원리 테스트

--- src/main/java/io/concurrency/chapter06/exam01/MutexExample.java
+++ src/main/java/io/concurrency/chapter06/exam01/MutexExample.java
@@ -4,9 +4,13 @@ public class MutexExample {
     public static void main(String[] args) throws InterruptedException {
         SharedData sharedData = new SharedData(new Mutex());
 
+
+        //스레드 1,2 생성
         Thread th1 = new Thread(sharedData::sum);
         Thread th2 = new Thread(sharedData::sum);
 
+
+        // 스레드 실행
         th1.start();
         th2.start();
 
--- src/main/java/io/concurrency/chapter06/exam01/SharedData.java
+++ src/main/java/io/concurrency/chapter06/exam01/SharedData.java
@@ -1,14 +1,21 @@
 package io.concurrency.chapter06.exam01;
 public class SharedData {
+
     private int sharedValue = 0;
+
     private Mutex mutex;
+
+
     public SharedData(Mutex mutex) {
         this.mutex = mutex;
     }
+
+    // 뮤텍스 lock 메커니즘 적용
     public void sum() {
         try {
             mutex.acquired(); // Lock 을 획득
 
+            // 임계 영역
             for (int i = 0; i < 10_000_0000; i++) {
                 sharedValue++;
             }
