a8a829351cf71bfbea13296628a8d36143dbb472
leeinhong <leeinhong9512@gmail.com>
2025-02-01 00:10:26 +0900

    feat: newCachedThreadPool 동작 원리 테스트

--- src/main/java/io/concurrency/chapter10/exam10/CachedThreadPoolExample.java
+++ src/main/java/io/concurrency/chapter10/exam10/CachedThreadPoolExample.java
@@ -6,8 +6,10 @@ import java.util.concurrent.Executors;
 public class CachedThreadPoolExample {
     public static void main(String[] args) {
 
+        // 스레드 풀 생성, 스레드의 개수를 제한하지 않으며 작업 요청이 많을 때는 스레드 수가 증가
         ExecutorService executorService = Executors.newCachedThreadPool();
 
+        // 작업 제출
         for (int i = 1; i <= 20; i++) {
             executorService.submit(() -> {
                 System.out.println("Thread : " + Thread.currentThread().getName());
--- src/main/java/io/concurrency/chapter10/exam10/CachedThreadPool_60_Example.java
+++ src/main/java/io/concurrency/chapter10/exam10/CachedThreadPool_60_Example.java
@@ -5,8 +5,11 @@ import java.util.concurrent.Executors;
 
 public class CachedThreadPool_60_Example {
     public static void main(String[] args) {
+
+        // 스레드 풀 생성
         ExecutorService executor = Executors.newCachedThreadPool();
 
+        // 작업 제출
         for (int i = 0; i < 10; i++) {
             final int taskId = i;
             executor.execute(() -> {
@@ -21,7 +24,7 @@ public class CachedThreadPool_60_Example {
             e.printStackTrace();
         }
 
-        // 60초 이상 동안 사용 되지 않은 스레드는 자동 종료됨
+        // 60초 이상 동안 사용 되지 않은 스레드는 자동 종료됨!
 
         executor.shutdown();
     }
